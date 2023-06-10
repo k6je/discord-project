b91ac129db568daba884b74513de412c1feaf301
Eray Kartal <97894113+k6je@users.noreply.github.com>
2023-06-11 02:23:14 +0300

    Create config.js

---
+++ Settings/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    tokens: [
+        "ODc2NTI2ODY4MzI3NzA2NjQ0.YRlXRw.lYnHViX71kKvWakthBlxolO9dk4",  
+    ], //Tokenlerinizi buraya girin
+
+    result: 0, //Elleme
+    maxUsers: 5, //Token basina MassDM atilacak kisi sayisi
+    captchaKey: "", //2captcha.com API Key (2captcha.com veya capmonster.cloud çalismaktadir)
+    captchaService: "", //Captcha Servisiniz (NOT: 2captcha veya capmonsterdan baska çalismaz)
+
+    proxys: [
+        "",
+    ], //Proxylerinizi buraya girin (NOT: http://kullaniciadi:sifre@ip:port olarak giriniz)
+
+    languages: ["tr", "en", "fr"], //Elleme
+    defaultLanguage: "en", //Dil secimi
+
+    message: {
+
+        content: `MESAJINI BURAYA GIR`, //Mass DM Mesaji
+
+    }
+
+}
