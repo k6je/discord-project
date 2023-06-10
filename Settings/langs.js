0f3d2b267494271a621eede06134e0862f55c3bc
Eray Kartal <97894113+k6je@users.noreply.github.com>
2023-06-11 02:23:30 +0300

    Add files via upload

---
+++ Settings/langs.js
@@ -0,0 +1,276 @@
+module.exports = {
+
+    tr: {
+
+        WELCOME: "Merhaba, hangi işlemi yapmak istersin?",
+        CLICK_BUTTON: "Butona tıklat",
+        REACT_TO_MESSAGE: "Mesaja tepki ver",
+        SEND_MESSAGE: "Mesaj gönder",
+        ADD_FRIEND: "Arkadaş isteği gönder",
+        CANCEL_FRIEND: "Arkadaş isteğini iptal et",
+        JOIN_SERVER: "Sunucuya katıl",
+        LEAVE_SERVER: "Sunucudan ayrıl",
+        MASS_DM: "Mass DM",
+        LANGUAGE: "Dil seçimi (Language selection)",
+        LANGUAGE_MESSAGE: "Dil seçimi yapmak için lütfen aşağıdaki butonlardan birini tıklayın.",
+        RESTARTING: "Yeniden başlatılıyor...",
+
+        MISSING_PARAMETERS: "Eksik parametreler: {parameters}",
+
+        LOGIN_ERROR: "Geçersiz Token veya Proxy: {token}",
+
+        HOW_MANY_USERS: "Kaç kişiyle yapılsın (Hepsi için 0 girin):",
+
+        CHANNEL_ID: "Kanal ID'sini girin:",
+        CHANNEL_ID_ERROR: "Lütfen geçerli bir kanal ID'si girin",
+
+        USER_OR_CHANNEL_ID: "Kullanıcı veya Kanal ID'sini girin:",
+        USER_OR_CHANNEL_ID_ERROR: "Lütfen geçerli bir kullanıcı veya kanal ID'si girin",
+
+        MESSAGE_ID: "Mesaj ID'sini girin:",
+        MESSAGE_ID_ERROR: "Lütfen geçerli bir mesaj ID'si girin",
+
+        BUTTON_ID: "Buton ID'sini girin (Buton pozisyonu):",
+        BUTTON_ID_ERROR: "Lütfen geçerli bir buton ID'si girin",
+
+        GUILD_ID: "Sunucu ID'sini girin:",
+        GUILD_ID_ERROR: "Lütfen geçerli bir sunucu ID'si girin",
+
+        TIME: "Süre (saniye):",
+
+        EMOJI: "Emoji:",
+        EMOJI_ERROR: "Lütfen geçerli bir emoji girin",
+
+        MESSAGE: "Mesaj:",
+        MESSAGE_ERROR: "Lütfen geçerli bir mesaj girin",
+
+        REPEAT: "Kaç kez tekrarlansın (0 = sonsuz):",
+        REPEAT_ERROR: "Lütfen geçerli bir sayı girin",
+
+        USER_ID: "Kullanıcı ID'sini girin:",
+        USER_ID_ERROR: "Lütfen geçerli bir kullanıcı ID'si girin",
+
+        NO_TOKENS: "Token bulunamadı. Lütfen token ekleyin.",
+
+        MESSAGE_NOT_FOUND: "Mesaj bulunamadı. ({clientUTAG})",
+        CHANNEL_NOT_FOUND: "Kanal bulunamadı. ({clientUTAG})",
+        SERVER_NOT_FOUND: "Sunucu bulunamadı. ({clientUTAG})",
+        USER_NOT_FOUND: "Kullanıcı bulunamadı. ({clientUTAG})",
+        ROW_NOT_FOUND: "Satır bulunamadı. ({clientUTAG})",
+        BUTTON_NOT_FOUND: "Buton bulunamadı. ({clientUTAG})",
+        EMOJI_NOT_FOUND: "Emoji bulunamadı. ({clientUTAG})",
+        USER_ALREADY_FRIEND: "Kullanıcı zaten arkadaş. ({clientUTAG})",
+        USER_ALREADY_SENT: "Kullanıcıya zaten arkadaş isteği gönderilmiş. ({clientUTAG})",
+        USER_NOT_SENT: "Kullanıcıya arkadaş isteği gönderilmemiş. ({clientUTAG})",
+        SERVER_ALREADY_JOINED: "Sunucuya zaten katılmış. ({clientUTAG})",
+        SERVER_ALREADY_LEFT: "Sunucudan zaten ayrılmış. ({clientUTAG})",
+        MASSDM_NOT_SENT: "{userTag} Kullanıcısına mesaj gönderilemedi. ({clientUTAG})",
+        MESSAGE_NOT_SEND: "Mesaj gönderilemedi. ({clientUTAG})",
+        SERVER_NOT_LEFT: "Sunucudan ayrılamadı. ({clientUTAG})",
+        SERVER_NOT_JOINED: "Sunucuya katılamadı. ({clientUTAG})",
+        FRIEND_REQUEST_NOT_SENT: "Arkadaş isteği gönderilemedi. ({clientUTAG})",
+        FRIEND_REQUEST_NOT_CANCELED: "Arkadaş isteği iptal edilemedi. ({clientUTAG})",
+
+        BUTTON_CLICKED: "Butona tıklandı. ({clientUTAG})",
+        FRIEND_REQUEST_SENT: "Arkadaş isteği gönderildi. ({clientUTAG})",
+        MESSAGE_SENT: "Mesaj gönderildi. ({clientUTAG})",
+        EMOJI_REACTED: "Emojiye tıklandı. ({clientUTAG})",
+        USERS_FOUND: "{users} Kullanıcı bulundu.",
+        SERVER_JOINED: "Sunucuya katıldı. ({clientUTAG})",
+        SERVER_LEFT: "Sunucudan ayrıldı. ({clientUTAG})",
+        MASSDM_SENT: "{userTAG} Kullanıcısına mesaj gönderildi. ({clientUTAG})",
+        FRIEND_REQUEST_CANCELED: "Arkadaş isteği iptal edildi. ({clientUTAG})",
+
+        TOTAL_BUTTON_CLICKED: "Toplam butona tıklayan kullanıcı: {result}",
+        TOTAL_FRIEND_REQUEST_SENT: "Toplam arkadaş isteği gönderen kullanıcı: {result}",
+        TOTAL_MESSAGE_SENT: "Toplam mesaj gönderen kullanıcı: {result}",
+        TOTAL_EMOJI_REACTED: "Toplam emojiye tıklayan kullanıcı: {result}",
+        TOTAL_SERVER_JOINED: "Toplam sunucuya katılan kullanıcı: {result}",
+        TOTAL_SERVER_LEFT: "Toplam sunucudan ayrılan kullanıcı: {result}",
+        TOTAL_FRIEND_REQUEST_CANCELED: "Toplam arkadaş isteği iptal eden kullanıcı: {result}",
+
+    },
+
+    en: {
+
+        WELCOME: "Hello, what do you want to do?",
+        CLICK_BUTTON: "Click button",
+        REACT_TO_MESSAGE: "React to message",
+        SEND_MESSAGE: "Send message",
+        ADD_FRIEND: "Send friend request",
+        CANCEL_FRIEND: "Cancel friend request",
+        JOIN_SERVER: "Join server",
+        LEAVE_SERVER: "Leave server",
+        MASS_DM: "Mass DM",
+        LANGUAGE: "Language selection",
+        LANGUAGE_MESSAGE: "Please click on one of the buttons below to select a language.",
+        RESTARTING: "Restarting...",
+
+        MISSING_PARAMETERS: "Missing parameters: {parameters}",
+
+        LOGIN_ERROR: "Invalid Token or Proxy: {token}",
+
+        HOW_MANY_USERS: "How many users (For all enter 0):",
+
+        CHANNEL_ID: "Enter channel ID:",
+        CHANNEL_ID_ERROR: "Please enter a valid channel ID",
+        
+        USER_OR_CHANNEL_ID: "Enter User or Channel ID:",
+        USER_OR_CHANNEL_ID_ERROR: "Please enter a valid user or channel ID",
+
+        MESSAGE_ID: "Enter message ID:",
+        MESSAGE_ID_ERROR: "Please enter a valid message ID",
+
+        BUTTON_ID: "Enter button ID (Button position):",
+        BUTTON_ID_ERROR: "Please enter a valid button ID",
+
+        GUILD_ID: "Enter server ID",
+        GUILD_ID_ERROR: "Please enter a valid server ID",
+
+        TIME: "Time (seconds):",
+
+        EMOJI: "Emoji:",
+        EMOJI_ERROR: "Please enter a valid emoji",
+
+        MESSAGE: "Message:",
+        MESSAGE_ERROR: "Please enter a valid message",
+
+        REPEAT: "How many times to repeat (0 = infinite):",
+        REPEAT_ERROR: "Please enter a valid number",
+
+        USER_ID: "Enter user ID:",
+        USER_ID_ERROR: "Please enter a valid user ID",
+
+        NO_TOKENS: "No tokens found. Please add a token.",
+
+        MESSAGE_NOT_FOUND: "Message not found. ({clientUTAG})",
+        CHANNEL_NOT_FOUND: "Channel not found. ({clientUTAG})",
+        SERVER_NOT_FOUND: "Server not found. ({clientUTAG})",
+        USER_NOT_FOUND: "User not found. ({clientUTAG})",
+        ROW_NOT_FOUND: "Row not found. ({clientUTAG})",
+        BUTTON_NOT_FOUND: "Button not found. ({clientUTAG})",
+        EMOJI_NOT_FOUND: "Emoji not found. ({clientUTAG})",
+        USER_ALREADY_FRIEND: "User already friend. ({clientUTAG})",
+        USER_ALREADY_SENT: "User already sent a friend request. ({clientUTAG})",
+        USER_NOT_SENT: "User not sent a friend request. ({clientUTAG})",
+        SERVER_ALREADY_JOINED: "Already joined the server. ({clientUTAG})",
+        SERVER_ALREADY_LEFT: "Already left the server. ({clientUTAG})",
+        MASSDM_NOT_SENT: "Message not sent to {userTag}. ({clientUTAG})",
+        MESSAGE_NOT_SEND: "Message not sent. ({clientUTAG})",
+        SERVER_NOT_LEFT: "Could not leave the server. ({clientUTAG})",
+        SERVER_NOT_JOINED: "Could not join the server. ({clientUTAG})",
+        FRIEND_REQUEST_NOT_SENT: "Friend request not sent. ({clientUTAG})",
+        FRIEND_REQUEST_NOT_CANCELED: "Friend request not cancelled. ({clientUTAG})",
+
+        BUTTON_CLICKED: "Button clicked. ({clientUTAG})",
+        FRIEND_REQUEST_SENT: "Friend request sent. ({clientUTAG})",
+        MESSAGE_SENT: "Message sent. ({clientUTAG})",
+        EMOJI_REACTED: "Emoji reacted. ({clientUTAG})",
+        USERS_FOUND: "{users} Users found.",
+        SERVER_JOINED: "Joined the server. ({clientUTAG})",
+        SERVER_LEFT: "Left the server. ({clientUTAG})",
+        MASSDM_SENT: "Message sent to {userTAG}. ({clientUTAG})",
+        FRIEND_REQUEST_CANCELED: "Friend request cancelled. ({clientUTAG})",
+
+        TOTAL_BUTTON_CLICKED: "Total users who clicked the button: {result}",
+        TOTAL_FRIEND_REQUEST_SENT: "Total users who sent friend request: {result}",
+        TOTAL_MESSAGE_SENT: "Total users who sent message: {result}",
+        TOTAL_EMOJI_REACTED: "Total users who reacted to emoji: {result}",
+        TOTAL_SERVER_JOINED: "Total users who joined the server: {result}",
+        TOTAL_SERVER_LEFT: "Total users who left the server: {result}",
+        TOTAL_FRIEND_REQUEST_CANCELED: "Total users who cancelled friend request: {result}",
+
+    },
+
+    fr: {
+
+        WELCOME: "Bonjour, que voulez-vous faire?",
+        CLICK_BUTTON: "Cliquez sur le bouton",
+        REACT_TO_MESSAGE: "Réagir au message",
+        SEND_MESSAGE: "Envoyer un message",
+        ADD_FRIEND: "Envoyer une demande d'amitié",
+        CANCEL_FRIEND: "Annuler la demande d'amitié",
+        JOIN_SERVER: "Rejoindre le serveur",
+        LEAVE_SERVER: "Quitter le serveur",
+        MASS_DM: "Mass DM",
+        LANGUAGE: "Sélection de la langue (Language selection)",
+        LANGUAGE_MESSAGE: "Veuillez cliquer sur l'un des boutons ci-dessous pour sélectionner une langue.",
+        RESTARTING: "Redémarrage...",
+
+        MISSING_PARAMETERS: "Paramètres manquants: {parameters}",
+
+        LOGIN_ERROR: "Token ou Proxy Invalide: {token}",
+
+        HOW_MANY_USERS: "Combien d'utilisateurs (Pour tous, entrez 0):",
+
+        CHANNEL_ID: "Entrez l'ID du canal:",
+        CHANNEL_ID_ERROR: "Veuillez entrer un ID de canal valide",
+
+        USER_OR_CHANNEL_ID: "Entrez l'ID de l'utilisateur ou du canal:",
+        USER_OR_CHANNEL_ID_ERROR: "Veuillez entrer un ID d'utilisateur ou de canal valide",
+
+        MESSAGE_ID: "Entrez l'ID du message:",
+        MESSAGE_ID_ERROR: "Veuillez entrer un ID de message valide",
+
+        BUTTON_ID: "Entrez l'ID du bouton (Position du bouton):",
+        BUTTON_ID_ERROR: "Veuillez entrer un ID de bouton valide",
+
+        GUILD_ID: "Entrez l'ID du serveur:",
+        GUILD_ID_ERROR: "Veuillez entrer un ID de serveur valide",
+
+        TIME: "Temps (secondes):",
+
+        EMOJI: "Emoji:",
+        EMOJI_ERROR: "Veuillez entrer un emoji valide",
+
+        MESSAGE: "Message:",
+        MESSAGE_ERROR: "Veuillez entrer un message valide",
+
+        REPEAT: "Combien de fois répéter (0 = infini):",
+        REPEAT_ERROR: "Veuillez entrer un nombre valide",
+
+        USER_ID: "Entrez l'ID de l'utilisateur:",
+        USER_ID_ERROR: "Veuillez entrer un ID d'utilisateur valide",
+
+        NO_TOKENS: "Aucun token trouvé. Veuillez ajouter des tokens.",
+
+        MESSAGE_NOT_FOUND: "Message introuvable. ({clientUTAG})",
+        CHANNEL_NOT_FOUND: "Canal introuvable. ({clientUTAG})",
+        SERVER_NOT_FOUND: "Serveur introuvable. ({clientUTAG})",
+        USER_NOT_FOUND: "Utilisateur introuvable. ({clientUTAG})",
+        ROW_NOT_FOUND: "Ligne introuvable. ({clientUTAG})",
+        BUTTON_NOT_FOUND: "Bouton introuvable. ({clientUTAG})",
+        EMOJI_NOT_FOUND: "Emoji introuvable. ({clientUTAG})",
+        USER_ALREADY_FRIEND: "Déjà ami. ({clientUTAG})",
+        USER_ALREADY_SENT: "Déjà envoyé une demande d'amitié. ({clientUTAG})",
+        USER_NOT_SENT: "N'a pas envoyé de demande d'amitié. ({clientUTAG})",
+        SERVER_ALREADY_JOINED: "Déjà rejoint le serveur. ({clientUTAG})",
+        SERVER_ALREADY_LEFT: "Déjà quitté le serveur. ({clientUTAG})",
+        MASSDM_NOT_SENT: "Message non envoyé à {userTag}. ({clientUTAG})",
+        MESSAGE_NOT_SEND: "Message non envoyé. ({clientUTAG})",
+        SERVER_NOT_LEFT: "Impossible de quitter le serveur. ({clientUTAG})",
+        SERVER_NOT_JOINED: "Impossible de rejoindre le serveur. ({clientUTAG})",
+        FRIEND_REQUEST_NOT_SENT: "Demande d'amitié non envoyée. ({clientUTAG})",
+        FRIEND_REQUEST_NOT_CANCELED: "Demande d'amitié non annulée. ({clientUTAG})",
+
+        BUTTON_CLICKED: "Bouton cliqué. ({clientUTAG})",
+        FRIEND_REQUEST_SENT: "Demande d'amitié envoyée. ({clientUTAG})",
+        MESSAGE_SENT: "Message envoyé. ({clientUTAG})",
+        EMOJI_REACTED: "Emoji réagi. ({clientUTAG})",
+        USERS_FOUND: "{users} Utilisateurs trouvés.",
+        SERVER_JOINED: "Rejoint le serveur. ({clientUTAG})",
+        SERVER_LEFT: "A quitté le serveur. ({clientUTAG})",
+        MASSDM_SENT: "Message envoyé à {userTAG}. ({clientUTAG})",
+        FRIEND_REQUEST_CANCELED: "Demande d'amitié annulée. ({clientUTAG})",
+
+        TOTAL_BUTTON_CLICKED: "Total des utilisateurs qui ont cliqué sur le bouton: {result}",
+        TOTAL_FRIEND_REQUEST_SENT: "Total des utilisateurs qui ont envoyé une demande d'amitié: {result}",
+        TOTAL_MESSAGE_SENT: "Total des utilisateurs qui ont envoyé un message: {result}",
+        TOTAL_EMOJI_REACTED: "Total des utilisateurs qui ont réagi à l'emoji: {result}",
+        TOTAL_SERVER_JOINED: "Total des utilisateurs qui ont rejoint le serveur: {result}",
+        TOTAL_SERVER_LEFT: "Total des utilisateurs qui ont quitté le serveur: {result}",
+        TOTAL_FRIEND_REQUEST_CANCELED: "Total des utilisateurs qui ont annulé la demande d'amitié: {result}",
+
+    }
+
+}
